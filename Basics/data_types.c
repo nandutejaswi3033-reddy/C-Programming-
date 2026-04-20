876349d1932488132203b04371a0cb4d0ff8c691
nandutejaswi3033-reddy <nandutejaswi3033@gmail.com>
2026-04-20 20:03:21 +0530

    data_types.c

---
+++ Basics/data_types.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    float b=3.5;
+    double c=50.455;
+    char d='T';
+    
+    printf("integer value:%d\n",a);
+    printf("float value:%f\n",b);
+    printf("double value:%lf\n",c);
+    printf("character value:%c\n",d);
+    return 0;
+}
+
+/*
+output:
+integer value:10
+float value:3.500000
+double value:50.455000
+character value:T
+*/
