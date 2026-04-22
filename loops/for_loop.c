669212fc0ea28ee9f87e0d50331f0b5899dd74cb
nandutejaswi3033-reddy <nandutejaswi3033@gmail.com>
2026-04-22 07:03:32 +0530

    Create for_loop.c

---
+++ loops/for_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=15;i++)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
+
+/*
+output:
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
+*/
