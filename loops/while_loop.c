7bd93b545188823b1d5d7d1b831766350dc197d8
nandutejaswi3033-reddy <nandutejaswi3033@gmail.com>
2026-04-22 07:19:16 +0530

    Create while_loop.c

---
+++ loops/while_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=18)
+    {
+        printf("%d ",i);
+        i++;
+    }
+}
+
+/*
+output:
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 
+*/
